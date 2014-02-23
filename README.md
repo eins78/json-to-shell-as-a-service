079f0441bebbdc588741e4734d7bbfe0426685f4
Max F. Albrecht <1@178.is>
2014-02-24 00:11:12 +0100

    first working protype

---
+++ README.md
@@ -0,0 +1,11 @@
+# `json-to-shell-as-a-service`
+
+A small web service converting `JSON` to `shell` variables.
+
+## API
+
+### `get`
+
+Get a resource from http and convert it.
+
+Example: `curl http://json2bash.ars.is/get?url=http://api.maschinenraum.tk/api/v0_12/status.json`
--- app.js
+++ app.js
@@ -1,13 +1,64 @@
 var flatiron = require('flatiron'),
     path = require('path'),
+    bash = require('bash-vars'),
+    curl = require('request'),
     app = flatiron.app;
 
 app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
 
 app.use(flatiron.plugins.http);
 
+//  home page
 app.router.get('/', function () {
-  this.res.json({ 'hello': 'world' })
+  this.res.json({ 
+    'hello': 'world'
+  });
 });
 
-app.start(3000);
+// 
+app.router.get(/\/get(.*)/, function (url) {
+
+  //console.log(url, this);
+  console.log(this.req.query.url)
+
+  var that = this,
+      url = false, err = false, res = false;
+
+  url = this.req.query.url;
+
+  if (!url) {
+    return that.res.end('Error: /get needs an url');
+  }
+
+  // get url
+  curl(url, function (error, response, body) {
+
+    if (error || (response && response.statusCode >= 400)) {
+      err = error || "http error";
+    };
+
+    if (!err && body) {
+      try {
+        res = JSON.parse(body);
+      }
+      catch (e) {
+        if (e) {};
+        err = 'JSON: ' + e;
+      }
+    }
+
+    if (err || !res) {
+      return that.res.end('Error ' + err);
+    } else {
+      console.log(res)
+      return that.res.end(bash.stringify(res) || "");
+    }
+
+  });
+});
+
+// start server
+var port = app.config.get("port");
+app.start(port, function () {
+  app.log.info("Server started on port %s!", port);
+});
